e26dc27d67d600f89c26611bfe695bcc08e4a983
Joel Håkansson <joel.hakansson@mtm.se>
2012-11-07 13:39:34 +0000

    changed to package-info.java

---
+++ braille/src/org/daisy/braille/ui/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * <p>Provides a framework for command line UI's and a simple implementation for core functionality in the org.daisy.braille packages.</p>
+ * <ul>
+ * 	<li>EmbossPEF</li>
+ * 	<li>PEFParser</li>
+ * <li>TextParser</li>
+ *	<li>ValidatePEF</li>
+ *	<li>SplitPEF</li>
+ *	<li>MergePEF</li>
+ *	<li>GeneratePEF</li>
+ * </ul>
+ * @author Joel Håkansson
+ */
+package org.daisy.braille.ui;
